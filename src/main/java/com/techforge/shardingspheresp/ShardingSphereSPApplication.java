b394c3577110d3b462f7e2d87b1172e9d6cb5d2e
YANLL <liangliang.yan@yeepay.com>
2019-02-19 16:45:03 +0800

    A

---
+++ src/main/java/com/techforge/shardingspheresp/ShardingSphereSPApplication.java
@@ -0,0 +1,22 @@
+package com.techforge.shardingspheresp;
+
+import com.techforge.shardingspheresp.configuration.FrameworkUtilConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by breez on 2016/03/30.
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {}, basePackageClasses = FrameworkUtilConfiguration.class)
+public class ShardingSphereSPApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereSPApplication.class, args);
+    }
+}
---
+++ src/main/java/com/techforge/shardingspheresp/configuration/FrameworkUtilConfiguration.java
@@ -0,0 +1,10 @@
+package com.techforge.shardingspheresp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({})
+public class FrameworkUtilConfiguration {
+
+}
